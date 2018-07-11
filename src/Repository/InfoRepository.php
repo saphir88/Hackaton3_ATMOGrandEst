0dd0c73738d0644457287e929ae60351519b895a
Xavier Vierling <xvrlng@gmail.com>
2018-07-11 21:54:23 +0200

    Bootstrap dans SCSS

---
+++ src/Repository/InfoRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Info;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Info|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Info|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Info[]    findAll()
+ * @method Info[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class InfoRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Info::class);
+    }
+}
