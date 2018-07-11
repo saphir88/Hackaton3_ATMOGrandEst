f46116d6acf68d482bae9a23ce61cf1bddc2632e
Thomas Catella <spectralys88@gmail.com>
2018-07-11 16:39:12 +0200

    creation de la BDD

---
+++ src/Controller/UtilisateurController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Utilisateur;
+use App\Form\UtilisateurType;
+use App\Repository\UtilisateurRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/utilisateur")
+ */
+class UtilisateurController extends Controller
+{
+    /**
+     * @Route("/", name="utilisateur_index", methods="GET")
+     */
+    public function index(UtilisateurRepository $utilisateurRepository): Response
+    {
+        return $this->render('utilisateur/index.html.twig', ['utilisateurs' => $utilisateurRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="utilisateur_new", methods="GET|POST")
+     */
+    public function new(Request $request): Response
+    {
+        $utilisateur = new Utilisateur();
+        $form = $this->createForm(UtilisateurType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($utilisateur);
+            $em->flush();
+
+            return $this->redirectToRoute('utilisateur_index');
+        }
+
+        return $this->render('utilisateur/new.html.twig', [
+            'utilisateur' => $utilisateur,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="utilisateur_show", methods="GET")
+     */
+    public function show(Utilisateur $utilisateur): Response
+    {
+        return $this->render('utilisateur/show.html.twig', ['utilisateur' => $utilisateur]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="utilisateur_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Utilisateur $utilisateur): Response
+    {
+        $form = $this->createForm(UtilisateurType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('utilisateur_edit', ['id' => $utilisateur->getId()]);
+        }
+
+        return $this->render('utilisateur/edit.html.twig', [
+            'utilisateur' => $utilisateur,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="utilisateur_delete", methods="DELETE")
+     */
+    public function delete(Request $request, Utilisateur $utilisateur): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$utilisateur->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($utilisateur);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('utilisateur_index');
+    }
+}
---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UtilisateurRepository")
+ */
+class Utilisateur
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $sexe;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $ville;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $age;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $sport;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mail;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getSexe(): ?string
+    {
+        return $this->sexe;
+    }
+
+    public function setSexe(string $sexe): self
+    {
+        $this->sexe = $sexe;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getAge(): ?string
+    {
+        return $this->age;
+    }
+
+    public function setAge(string $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    public function getSport(): ?bool
+    {
+        return $this->sport;
+    }
+
+    public function setSport(?bool $sport): self
+    {
+        $this->sport = $sport;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(string $mail): self
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+}
