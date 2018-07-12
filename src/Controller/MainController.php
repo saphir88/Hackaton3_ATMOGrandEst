e400f5f0e1b57314445ee73c0f0b321201dc6e43
Xavier Vierling <xvrlng@gmail.com>
2018-07-12 13:59:56 +0200

    Formulaire OK

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,9 +8,14 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Users;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 
 /**
@@ -23,8 +28,32 @@ class MainController extends Controller
     /**
      * @Route("/", name="default_index", methods="GET")
      */
-    public function indexAction(){
-        return $this->render('base.html.twig');
+    public function indexAction(Request $request){
+
+        $user = new Users();
+
+        $form = $this->createFormBuilder($user)
+            ->add('email', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Enregistrer'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $task = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            // $entityManager = $this->getDoctrine()->getManager();
+            // $entityManager->persist($task);
+            // $entityManager->flush();
+        }
+
+            return $this->render('base.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
     /**
      * @Route("/accueil", name="accueil", methods={"GET", "POST"})
@@ -35,5 +64,6 @@ class MainController extends Controller
         return $this->render('accueil/accueil.html.twig',array(
             'test' => $test,
     ));
+
     }
 }
