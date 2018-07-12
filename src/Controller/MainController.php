5e36af63b78ffce7d87f568d7609ee71433f4079
Thomas Catella <spectralys88@gmail.com>
2018-07-12 14:16:59 +0200

    intauration de l'ajax

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,10 +9,13 @@
 namespace App\Controller;
 
 
+use App\Entity\Info;
+use phpDocumentor\Reflection\DocBlock\Serializer;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
 
 
 /**
@@ -32,38 +35,20 @@ class MainController extends Controller
     /**
      * @Route("/test", name="test_index", methods="GET")
      */
-    public function test(Request $request){
-        // Array with names
-        $a[] = "Anna";
-        $a[] = "Brittany";
-        $a[] = "Cinderella";
-        $a[] = "Diana";
-        $a[] = "Eva";
-        $a[] = "Fiona";
-        $a[] = "Gunda";
-        $a[] = "Hege";
-        $a[] = "Inga";
-        $a[] = "Johanna";
-        $a[] = "Kitty";
-        $a[] = "Linda";
-        $a[] = "Nina";
-        $a[] = "Ophelia";
-        $a[] = "Petunia";
-        $a[] = "Amanda";
-        $a[] = "Raquel";
-        $a[] = "Cindy";
-        $a[] = "Doris";
-        $a[] = "Eve";
-        $a[] = "Evita";
-        $a[] = "Sunniva";
-        $a[] = "Tove";
-        $a[] = "Unni";
-        $a[] = "Violet";
-        $a[] = "Liza";
-        $a[] = "Elizabeth";
-        $a[] = "Ellen";
-        $a[] = "Wenche";
-        $a[] = "Vicky";
+    public function test(Request $request, SerializerInterface $serializer){
+
+        $commune = $this->getDoctrine()->getManager()
+            ->getRepository(Info::class)->commune();
+
+        $data = $commune->getArrayResult();
+
+        foreach($data as $key => $value){
+            foreach ($value as $test){
+                $tableau[]=$test;
+            }
+        }
+
+
 
 // get the q parameter from URL
         $q = $request->query->get('q');
@@ -74,7 +59,7 @@ class MainController extends Controller
         if ($q !== NULL) {
             $q = strtolower($q);
             $len=strlen($q);
-            foreach($a as $name) {
+            foreach($tableau as $name) {
                 if (stristr($q, substr($name, 0, $len))) {
                     if ($hint === "") {
                         $hint = $name;
@@ -87,6 +72,6 @@ class MainController extends Controller
         }
 
 // Output "no suggestion" if no hint was found or output correct values
-        return $this->render('test.html.twig');
+        return $this->render('test.html.twig',['tableau'=> $tableau]);
     }
 }
