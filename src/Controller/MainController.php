e9e3951e1ee96cb77aec49878409637c85b46998
Cyril Bellamy <cyrilbellamy1@gmail.com>
2018-07-12 16:29:39 +0200

    meteo

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -90,9 +90,61 @@ class MainController extends Controller
              $entityManager->persist($user);
              $entityManager->flush();
         }
-        return $this->render('accueil/accueil.html.twig', array(
+
+
+        /* METEO */
+
+        $commune = $this->getDoctrine()->getManager()
+            ->getRepository(Info::class)->commune();
+
+        $data = $commune->getArrayResult();
+
+        foreach($data as $key => $value){
+            foreach ($value as $test){
+                $tableau[]=$test;
+            }
+        }
+
+        // get the q parameter from URL
+        $q = $request->query->get('q');
+
+        $hint = "";
+
+        // lookup all hints from array if $q is different from ""
+        if ($q !== NULL) {
+            $q = strtolower($q);
+            $len=strlen($q);
+            foreach($tableau as $name) {
+                if (stristr($q, substr($name, 0, $len))) {
+                    if ($hint === "") {
+                        $hint = $name;
+                    } else {
+                        $hint .= ", $name";
+                    }
+                }
+            }
+            return new Response($hint);
+        }
+
+        if (!empty($_POST)) {
+            $ville = $_POST['ville'];
+            $donnees = $this->getDoctrine()->getManager()
+                ->getRepository(Info::class)->findOneBy(['commune'=>$ville]);
+
+
+            $api = 'https://api.apixu.com/v1/forecast.json?key=ea49c70579214bf1b16204559181107&q=' . $ville . '&lang=fr';
+            $response = file_get_contents($api);
+            $jsonobj = json_decode($response);
+        }else{
+            $jsonobj = [];
+            $donnees = [];
+        }
+        // Output "no suggestion" if no hint was found or output correct values
+        return $this->render('accueil/accueil.html.twig',['tableau'=> $tableau,
+            'open' => $jsonobj,
             'form' => $form->createView(),
-        ));
+            'donnees' => $donnees]);
+
     }
 
     /**
