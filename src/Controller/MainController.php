ac8404f4632e8386bacf29e8b4ccc2d9958f8254
Thomas Catella <spectralys88@gmail.com>
2018-07-12 10:46:58 +0200

    a

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,7 +10,9 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 
 /**
@@ -25,5 +27,66 @@ class MainController extends Controller
      */
     public function indexAction(){
         return $this->render('base.html.twig');
+    }
+
+    /**
+     * @Route("/test", name="test_index", methods="GET")
+     */
+    public function test(Request $request){
+        // Array with names
+        $a[] = "Anna";
+        $a[] = "Brittany";
+        $a[] = "Cinderella";
+        $a[] = "Diana";
+        $a[] = "Eva";
+        $a[] = "Fiona";
+        $a[] = "Gunda";
+        $a[] = "Hege";
+        $a[] = "Inga";
+        $a[] = "Johanna";
+        $a[] = "Kitty";
+        $a[] = "Linda";
+        $a[] = "Nina";
+        $a[] = "Ophelia";
+        $a[] = "Petunia";
+        $a[] = "Amanda";
+        $a[] = "Raquel";
+        $a[] = "Cindy";
+        $a[] = "Doris";
+        $a[] = "Eve";
+        $a[] = "Evita";
+        $a[] = "Sunniva";
+        $a[] = "Tove";
+        $a[] = "Unni";
+        $a[] = "Violet";
+        $a[] = "Liza";
+        $a[] = "Elizabeth";
+        $a[] = "Ellen";
+        $a[] = "Wenche";
+        $a[] = "Vicky";
+
+// get the q parameter from URL
+        $q = $request->query->get('q');
+
+        $hint = "";
+
+// lookup all hints from array if $q is different from ""
+        if ($q !== NULL) {
+            $q = strtolower($q);
+            $len=strlen($q);
+            foreach($a as $name) {
+                if (stristr($q, substr($name, 0, $len))) {
+                    if ($hint === "") {
+                        $hint = $name;
+                    } else {
+                        $hint .= ", $name";
+                    }
+                }
+            }
+            return new Response($hint);
+        }
+
+// Output "no suggestion" if no hint was found or output correct values
+        return $this->render('test.html.twig');
     }
 }
